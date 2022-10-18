e3e8419d1f9a22a808dfb2da7c4ddeb92887a5e3
Denis Souza <denis.souza@ulteriorti.com.br>
2022-10-18 11:16:48 -0300

    Implement swagger security configuration

--- src/main/java/br/com/ulteriorti/parking/config/SecurityConfiguration.java
+++ src/main/java/br/com/ulteriorti/parking/config/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package br.com.ulteriorti.parking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,7 +24,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .passwordEncoder(passwordEncoder());
     }
+@Override
+protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/swagger-ui/**")
+                .permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v2/api-docs/**").permitAll()
+                .antMatchers("/*.js").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/csrf").permitAll()
+                .antMatchers("/*.css").permitAll()
+                .antMatchers("/*.png").permitAll()
+                .antMatchers("/*.ico").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+}
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
